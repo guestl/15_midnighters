3d228e7751a38f12a92a9fd4b87a6c7259b968eb
guestl <boris.nikulin@gmail.com>
2017-10-10 22:27:40 +0300

    First release

--- README.md
+++ README.md
@@ -1,6 +1,50 @@
 # Night Owls Detector
 
-[TODO. There will be project description]
+Devman keeps data about username, date, time and timezone of people who post task for checking.
+
+This data is available from http://devman.org/api/challenges/solution_attempts/?page=2 
+
+The script loads data and print usernames who made post at midnigth
+
+# Quickstart
+
+Example of script launch on Linux, Python 3.5:
+
+```#!bash
+
+$ python seek_dev_nighters.py
+lekcrg
+roman_muzzy
+AlexBald
+AlexBald
+madfoolish
+roman_muzzy
+roman_muzzy
+roman_muzzy
+roman_muzzy
+roman_muzzy
+roman_muzzy
+roman_muzzy
+smth_creative
+b0r1soff
+id5616980
+id5616980
+rzabaluev
+rzabaluev
+АлексейЦыкунов
+irinanizova0
+irinanizova0
+chadarr
+irinanizova0
+АлексейЦыкунов
+lekcrg
+lekcrg
+mmatveev
+lekcrg
+irinanizova0
+mmatveev
+```
+
 
 # Project Goals
 
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,61 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import requests
+import json
+import pytz
+from datetime import datetime
 
-def get_midnighters():
-    pass
+
+def load_single_page(page_number):
+    url = 'http://devman.org/api/challenges/solution_attempts/'
+    result = None
+
+    payloads = {'page': page_number}
+
+    req = requests.get(url, params=payloads)
+
+    try:
+        if req.status_code == requests.codes.ok:
+            result = json.loads(req.text)['records']
+        return result
+    except ValueError:
+        exit("Error with content of the page {}".format(req.url))
+
+
+def load_attempts():
+    pages = 10
+    for page in range(1, pages):
+        single_page_dict = load_single_page(page)
+
+        if not single_page_dict:
+            exit('Error with load page {}'.format(page))
+
+        for item in single_page_dict:
+            yield {
+                'username': item['username'],
+                'timestamp': item['timestamp'],
+                'timezone': item['timezone'],
+            }
+
+
+def get_midnighters(single_record, morning_hour):
+    midnighters_hours = {hour for hour in range(morning_hour)}
+    result = None
+
+    if single_record['timestamp'] and single_record['timezone']:
+        user_tz = pytz.timezone(single_record['timezone'])
+        user_utc_time = single_record['timestamp']
+
+        user_local_time = pytz.utc.localize(datetime.utcfromtimestamp(
+            user_utc_time), is_dst=None).astimezone(user_tz)
+
+        if user_local_time.hour in midnighters_hours:
+            result = single_record['username']
+
+    return result
+
 
 if __name__ == '__main__':
-  pass
+    morning_hour = 5
+    for item in load_attempts():
+        midnighter = get_midnighters(item, morning_hour)
+        if midnighter:
+            print(midnighter)
